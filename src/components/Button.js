51db2446fff0721a55469389c1c0f3f558a4c1eb
unknown <thitamepankaj@gmail.com>
2025-05-18 01:23:05 +0530

    first combine 3

--- src/components/Button.js
+++
@@ -1,6 +0,0 @@
-const Button = ({ children, onClick }) => (
-    <button onClick={onClick}>{children}</button>
-  );
-  
-  export default Button;
-  
--- src/components/Input.js
+++
@@ -1,6 +0,0 @@
-const Input = ({ value, onChange, placeholder }) => (
-    <input value={value} onChange={onChange} placeholder={placeholder} />
-  );
-  
-  export default Input;
-  
